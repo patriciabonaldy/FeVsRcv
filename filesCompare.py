781ac69affc2cfcba3baeb5a174b75693f917001
Daniel Quintana <dquintana@makro.com.ar>
2019-06-26 23:04:13 -0300

    se agregan todos los campos en el merge

--- filesCompare.py
+++ filesCompare.py
@@ -14,18 +14,18 @@ from IPython.display import display
 from IPython.display import Image
 
 #Importar la data
-urlFE = 'C:\\repositorio\\FeVsRcv\\datos\\redmine_52270_fe.txt'
-urlRCV = 'C:\\repositorio\\FeVsRcv\\datos\\redmine_52270_rcv.txt'   
-urlFinal = 'C:\\repositorio\\FeVsRcv\\datos\\redmine_52270_join.txt'         
+urlFE = 'C:\\Users\\dquintana\\pythonproect\\FeVsRcv\\datos\\redmine_52270_fe.txt'
+urlRCV = 'C:\\Users\\dquintana\\pythonproect\\FeVsRcv\\datos\\redmine_52270_rcv.txt'   
+urlFinal = 'C:\\Users\\dquintana\\pythonproect\\FeVsRcv\\datos\\redmine_52270_join.txt'         
     
 df_FE = pd.read_csv(urlFE, delimiter=';')
 df_RCV = pd.read_csv(urlRCV, delimiter=';')
-print(df_FE.head(3))
+print(df_FE)
 print("------------------------")
-print(df_RCV.head(3))
+print(df_RCV)
 print("------------------------")
-df_inner = pd.merge(df_FE, df_RCV, on='cuit_comprador', how='inner', suffixes=('_left','_right'))
-print(df_inner.head(3))
+df_inner = pd.merge(df_FE, df_RCV, on=['cuit_comprador','cuit_vendedor','fecha_emision_comprobante','tipo_cbte','nro_cbte_desde','nro_cbte_hasta','punto_de_venta','moneda','cae','importe_total'], how='inner', suffixes=('_left','_right'))
+print(df_inner)
 
 
            
